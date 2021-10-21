97f5d2b581030f1818396192999c4c44660f57b0
Mons Joseph <monsejoseph5@gmail.com>
2021-10-21 17:16:58 +0530

    Views all log files rather than just one and moved regex implementation to client side.

--- script1.js
+++ script1.js
@@ -1,35 +1,76 @@
 // http://localhost/C:/Users/monse/AppData/Local/anywhere.app-logs/17-Oct-2021/13-00/AnySnap.html-renderer
 const http = require("http");
 const fs = require("fs");
-let url = '';
-let path = '';
+const emitter = require('events').EventEmitter;
+
+
+let dateArray = fs.readdirSync("C:\\Users\\monse\\AppData\\Local\\anywhere.app-logs");
+
+let timeArray = (() => {
+    var x = [];
+    for (dateFolder of dateArray) {
+        x.push(fs.readdirSync(`C:\\Users\\monse\\AppData\\Local\\anywhere.app-logs\\${dateFolder}`));
+    }
+    return x;
+})();
+
+let fileNamesArray = (() => {
+    var x = [];
+    for (let i = 0; i < dateArray.length; i++) {
+        if (timeArray[i].length == 1) {
+            x.push(fs.readdirSync(`C:\\Users\\monse\\AppData\\Local\\anywhere.app-logs\\${dateArray[i]}\\${timeArray[i]}`));
+        } else if (timeArray[i].length > 1) {
+            for (let j = 0; j < timeArray[i].length; j++) {
+                x.push(fs.readdirSync(`C:\\Users\\monse\\AppData\\Local\\anywhere.app-logs\\${dateArray[i]}\\${timeArray[i][j]}`));
+            }
+        }
+    }
+    return x;
+})();
+
+// array containing the exact path to each .log files.
+let pathArray = (() => {
+    var x = [];
+    for (let i = 0; i < dateArray.length; i++) {
+        if (timeArray[i].length == 1 && fileNamesArray[i].length >= 1) {
+            x.push(`C:\\Users\\monse\\AppData\\Local\\anywhere.app-logs\\${dateArray[i]}\\${timeArray[i]}\\${fileNamesArray[i]}`);
+        } else if (timeArray[i].length > 1) {
+            for (let j = 0; j < timeArray[i].length; j++) {
+                for (let k = 0; k < fileNamesArray.length; k++) {
+                    for (let l = 0; l < fileNamesArray[k].length; l++) {
+                        // console.log(" i:",i, " j:",j, " k:",k, " l:",l)
+                        if (x.indexOf(`C:\\Users\\monse\\AppData\\Local\\anywhere.app-logs\\${dateArray[i]}\\${timeArray[i][j]}\\${fileNamesArray[k][l]}`) == -1) {
+                            x.push(`C:\\Users\\monse\\AppData\\Local\\anywhere.app-logs\\${dateArray[i]}\\${timeArray[i][j]}\\${fileNamesArray[k][l]}`);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return x;
+})();
+
+// to remove any file path that does not have a .log extention.
+for(let i = 0; i < pathArray.length; i++){
+    if(pathArray[i].length > 200 || pathArray[i].match(/zip/)){
+        pathArray.splice(i, 1);
+        
+    }
+}
+
+
 
 http.createServer((req, res) => {
-    let tableString = "";
-
-    if (req.url != "/stringData") {
-        url = (req.url + ".log").replace(/\//, "");
-        path = decodeURIComponent(url.replace(/\//g, "%5C%5C"));
+ 
+    if (req.url === "/") {
         fs.createReadStream("D:\\Mons\\Code Projects\\Node\\Task3-Show_logs\\index.html").pipe(res);
     }
 
-    if (req.url == "/stringData") {
-        let myFile = ""
-        let stream = fs.createReadStream(path);
-        stream.on("data", (chunk) => {
-            myFile += chunk;
-        });
-        stream.on("end", () => {
-            // let dateArray = myFile.match(/\d\d[/]\d\d[/]\d\d/g);
-            let timeArray = myFile.match(/\d\d[:]\d\d[:]\d\d/g);
-            let logLevelArray = myFile.match(/error|warn|info|verbose|debug|silly/g);
-            let logValueArray = myFile.match(/(?<=\:\')(.*)(?=\')/g);
-
-            for (let i = 0; i < timeArray.length; i++) {
-                tableString += "<tr>" + "<td>" + timeArray[i] + "</td>" + "<td>" + logLevelArray[i] + "</td>" + "<td>" + logValueArray[i] + "</td>" + "</tr>";
-            }
-
-            res.end(tableString);
-        });
+    if (req.url === "/stringData") {
+        // to remove change maxListener error throwing from createReadStream.pipe().
+        emitter.setMaxListeners(pathArray.length); 
+        for (let i = 1; i < pathArray.length; i++) {
+            fs.createReadStream(pathArray[i]).pipe(res);
+        }
     }
 }).listen(80);
